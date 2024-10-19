4f0118052f324bfe1f62dc716e50a5d6fba384bb
Muhammad Qasim <kingqasimmalik8@gmail.com>
2024-10-19 16:09:13 +0500

    06currencyConverter Updated

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -5,23 +5,23 @@ import '/Downloader-La-974610.jpg';
 import './App.css';
 
 function App() {
-  const [amount, setAmount] = useState(0);
-  const [from, setFrom] = useState('usd');
-  const [to, setTo] = useState('inr');
-  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [amount, setAmount] = useState(0);                      /* Using State for set amount */
+  const [from, setFrom] = useState('usd');                      /* Using State for set From Currency */
+  const [to, setTo] = useState('inr');                          /* Using State for set To Currency */
+  const [convertedAmount, setConvertedAmount] = useState(0);    /* Using State for set Converted Amount */
   const currencyInfo = useCurrencyInfo(from);
   const options = Object.keys(currencyInfo);
 
-  const swap = () => {
+  const swap = () => {                  /* Creating Swap Function to Swap Values */
     setFrom(to);
     setTo(from);
     setConvertedAmount(amount);
     setAmount(convertedAmount);
   };
 
-  const convert = () => {
+  const convert = () => {               /* Creating Convert Function to convert */
     if (currencyInfo[to]) {
-      setConvertedAmount(amount * currencyInfo[to]);
+      setConvertedAmount(amount * currencyInfo[to]);    /* Multiply {From} with {to} if exists */
     }
   };
 
@@ -29,25 +29,25 @@ function App() {
     <div
       className="w-full h-screen bg-black flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('/Downloader-La-974610.jpg')`, // Corrected image path
+        backgroundImage: `url('/Downloader-La-974610.jpg')`,    /* Setting Background Image */
       }}
     >
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
             onSubmit={(e) => {
-              e.preventDefault();
-              convert();
+              e.preventDefault();               /* Using Prevent Default on Form Submit */
+              convert();                        /* Calling convert function on Form Submit */
             }}
           >
             <div className="w-full mb-1">
-              <InputBox
-                label="From"
-                amount={amount}
-                currencyOptions={options}
-                onCurrencyChange={(currency) => setFrom(currency)}
-                selectCurrency={from}
-                onAmountChange={(amount) => setAmount(amount)}
+              <InputBox                         /* Creating From Input From Inputbox */
+                label="From"                    /* Label */
+                amount={amount}                 /* Setting amount from state */
+                currencyOptions={options}       /* Setting Currency Options got from API */
+                onCurrencyChange={(currency) => setFrom(currency)}   /* Setting Currency to {From} */
+                selectCurrency={from}           /* Setting Currency from the {from} */
+                onAmountChange={(amount) => setAmount(amount)}  /* Set New Amount in the state */
               />
             </div>
             <div className="relative w-full h-0.5">
@@ -62,10 +62,10 @@ function App() {
             <div className="w-full mt-1 mb-4">
               <InputBox
                 label="To"
-                amount={convertedAmount}
-                currencyOptions={options}
-                onCurrencyChange={(currency) => setTo(currency)}
-                selectCurrency={to}
+                amount={convertedAmount}        /* Setting Convertedamount from state to {to}*/
+                currencyOptions={options}       /* Setting Currency Options got from API */
+                onCurrencyChange={(currency) => setTo(currency)} /* Setting Currency to {to} */
+                selectCurrency={to}             /* Setting Currency from the {to} */
                 amountDisable
               />
             </div>
--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -18,15 +18,15 @@ function InputBox({
         <div className={`bg-white p-3 rounded-lg text-sm flex `}>
             <div className="w-1/2">
                 <label  className="text-black/40 mb-2 inline-block" htmlFor={amountInputID}>
-                    {label}
+                    {label}  {/* Using Lable Here */}
                 </label>
                 <input
                     id={amountInputID}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
-                    disabled= {amountDisable}
-                    value={amount}
+                    disabled= {amountDisable}   /* Using Amount Distable */
+                    value={amount}              /* Using Amount as the value of Input */
                     onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
@@ -34,12 +34,13 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    value={selectCurrency}
+                    value={selectCurrency}      /* Showing Currency */
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                 >
+                        {/* Loop the currency option */}
                         {currencyOptions.map((currency)=> (
-                            <option key={currency} value={currency}>
+                            <option key={currency} value={currency}> 
                             {currency}
                             </option>
                         ))}
