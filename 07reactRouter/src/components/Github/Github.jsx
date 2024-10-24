96cc324812acc0ee2cf6c1b103eca24c11bf9b47
Muhammad Qasim <kingqasimmalik8@gmail.com>
2024-10-24 19:51:29 +0500

    Complete 07reactRouter

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 export default function Github(){
+    const [data,setData] = useState([])
+    useEffect(()=>{
+        fetch('https://api.github.com/users/Muhammad-Qasim-Malik')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setData(data)
+        })
+    },[])
     return(
         <div className="bg-gray-600 text-center m-4 text-white p-4 text-2xl">
-            Github Followers: 
+            Github Followers: {data.followers}
+            <img src={data.avatar_url} alt="Git Picture" className="rounded"/>
         </div>
     )
 }
